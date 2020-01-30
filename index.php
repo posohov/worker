91f3f5d1c05fa595d05ca1cff220ea059c424d71
Yurii Posohov <mangura3078@gmail.com>
2020-01-30 22:14:20 +0200

    Yurii

--- index.php
+++ index.php
@@ -1,13 +1,61 @@
 <?php
-$con = mysqli_connect('localhost', 'root', '', 'worker');
+$connect = mysqli_connect('localhost', 'root', '', "worker");
+mysqli_set_charset($connect, "utf8");
 if(mysqli_connect_errno()) {
-    echo 'error' . mysqli_connect_error();
+    echo "error" . mysqli_connect_error();
 } else {
     echo 'Ура';
 }
-//$str = "SELECT * FROM worker;";
-//$res = mysqli_query($con, $str);
-//$count = mysqli_num_rows($res);
+//$query = "UPDATE `users` SET name='George' WHERE id='3';";
+//$res = mysqli_query($connect, $query);
+$str = "SELECT * FROM users;";
+$res = mysqli_query($connect, $str);
+var_dump($res);
+$count = mysqli_num_rows($res);
+var_dump($count);
+$row1 = mysqli_fetch_array($res);
+$row2 = mysqli_fetch_array($res);
+$row3 = mysqli_fetch_assoc($res);
+print_r($row3) . '<br>';
+
+print_r($row1);
+print_r($row2);
+
+echo '<hr>';
+echo '<hr>';
+
+$con = mysqli_connect("localhost", "root", "", "testsite");
+mysqli_set_charset($con, "utf8");
+
+if(mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL" . mysqli_connect_error();
+}
+/*$query = "INSERT INTO `news_category` VALUES (NULL, 'Категория 50', 'Описание 6', '1', '1'), (NULL, 'Категория 51', 'Описание 7', '1', '1'), (NULL, 'Категория 42', 'Описание 52', '1', '1') ;";*/
+$query = "UPDATE `news` SET h1='Новый заголовок 3' WHERE id='1';";
+$info = mysqli_query($con, $query);
+var_dump($info);
+echo "Hello world";
+echo '<br>';
+// echo mysqli_affected_rows($con);
+$query = "SELECT * FROM `news`;";
+$result = mysqli_query($con, $query);
+$count =mysqli_num_rows($result);
+echo $count;
+// $row1 = mysqli_fetch_array($result);
+// $row2 = mysqli_fetch_array($result);
+// echo '<pre>';
+// print_r($row1);
+// print_r($row2);
+echo "<pre>";
+if($count) {
+    while($row = mysqli_fetch_array($result)) {
+        echo '<p>';
+        echo $row['h1'];
+        echo '</p>';
+
+    }
+}
+// var_dump($info);
 ?>
 
 
